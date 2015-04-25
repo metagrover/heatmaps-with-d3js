61d194fd30b094333c51251c6e1621c2a5e5dbca
prasad_tambekar <iamprasadt09@gmail.com>
2015-04-25 15:17:15 +0530

    Add ngmaps

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-var app = angular.module('housing', ['ui.router', 'uiGmapgoogle-maps']);
-app.config(['uiGmapGoogleMapApiProvider',function(uiGmapGoogleMapApiProvider){
-    uiGmapGoogleMapApiProvider.configure({
-        india: true
-    });
-}]);
-
-var data = [{lat: 28.6139, lon: 77.2090},
-    {lat: 12.9667, lon: 77.5667},
-    {lat: 17.3700, lon: 78.4800}];
+var app = angular.module('housing', ['ui.router', 'ngMap']);
     
 app.controller('baseController', ['$scope', function ($scope) {
-    $scope.location = {};
-    $scope.map = { center: {latitude: 28.6139, longitude:77.2090 } , 
-                     zoom: 8,
-                     options: { scrollwheel: false,
-                        disableDefaultUI: true } 
-                };
-    $scope.resetMap = function(){
-        var name = $scope.location.id;
-        $scope.map = { center: {latitude: data[name].lat, longitude: data[name].lon }
-                  };
-    }
+    
 }]);
